067dac8947b7d12dd9adf07048ab7ac93475058d
reza.souri@gmail.com <reza.souri@gmail.com>
2022-04-10 23:37:21 +1200

    adding scanner

--- screens/scanning/Scanning.js
+++ screens/scanning/Scanning.js
@@ -1,20 +1,89 @@
-import React, { useEffect } from "react";
-import { StyleSheet, FlatList, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, FlatList, View, Text, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import Colors from "../../constants/Colors";
 
 const Scanning = (props) => {
-  // useEffect(() => {
-  //   props.navigation.setOptions({
-  //     headerShown: false,
-  //   });
-  // });
+  const [hasPermission, setHasPermission] = useState(null);
+  const [text, setText] = useState("Not Scan Yet");
+  const [scanned, setScanned] = useState(false);
 
+  const askForPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  };
+  useEffect(() => {
+    askForPermission();
+    //   props.navigation.setOptions({
+    //     headerShown: false,
+    //   });
+  }, []);
+  const handleBarCodeScanned = ({ type, data }) => {
+    setText(data);
+    setScanned(true);
+    console.log(`type: ${type} \n data: ${data}`);
+  };
+  if (hasPermission === null)
+    return (
+      <View style={styles.center}>
+        <Text>Request for camera permission</Text>
+      </View>
+    );
+  if (hasPermission === false)
+    return (
+      <View style={styles.center}>
+        <Text>No Access to Camer.</Text>
+
+        <Button
+          onPress={() => askForPermission()}
+          title="Allow use Camer"
+        ></Button>
+      </View>
+    );
   return (
-    <View>
-      <Text>Scanning</Text>
+    <View style={styles.center}>
+      <View style={styles.barCoeBox}>
+        {!scanned && (
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={{ height: 400, width: 400 }}
+          />
+        )}
+      </View>
+      <Text style={styles.mainText}>{text}</Text>
+      {scanned && (
+        <View>
+          <Button
+            title="Scan Again?"
+            onPress={() => setScanned(false)}
+          ></Button>
+        </View>
+      )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  barCoeBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: Colors.accentColor,
+  },
+  mainText: {
+    fontSize: 16,
+    margin: 50,
+  },
+});
 
 export default Scanning;
