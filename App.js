200dda3c518cd06f44061274e11eeda5d25a4cab
Reza Soori <reza.souri@gmail.co>
2023-07-20 12:28:45 +1200

    1.0.6

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import MyTabs from "./navigation";
 import {init} from "./helper/db";
-
 import React from "react";
 
 init().then(()=>{
@@ -15,11 +12,4 @@ export default function App() {
   return <MyTabs />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
